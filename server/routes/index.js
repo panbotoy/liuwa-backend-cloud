bcd0fbc9c7e2207c27860d6266164157284a1c2a
Bo Pan <bopan@bos-macbook-pro.lan>
2018-09-03 23:05:45 -0700

    [liuwa-backend] added a member controller and proudly overwrote Paopao's deployment~~~

--- server/routes/index.js
+++ server/routes/index.js
@@ -32,4 +32,7 @@ router.get('/message', controllers.message.get)
 // POST 用来处理微信转发过来的客服消息
 router.post('/message', controllers.message.post)
 
+// a controller for member related APIs
+router.get('/member', controllers.member)
+
 module.exports = router
