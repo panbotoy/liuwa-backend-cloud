93aecdeb8af0daf5d089c8f9e869d967cb7e22d0
Bo Pan <bopan@bos-macbook-pro.lan>
2018-09-09 11:43:29 -0700

    tried to make sure DEV and EI environment works automatically without switching

--- server/controllers/member.js
+++ server/controllers/member.js
@@ -1,11 +1,14 @@
-const { mysql } = require('../qcloud')
+// const { mysql } = require('../qcloud')
 const bodyParser = require('body-parser')
 const Member = require('../models/member.js').Member
+const dbConfig = require('../database-config.js')
+const knex = require('knex')(dbConfig.CURRENT_ENV())
+const hostname = require('os').hostname();
 
 async function get(ctx, next) {
   var id = parseFloat(ctx.params.id);
-  console.log(`Trying to fetch member with id ` + id)
-  await mysql('member').select('*').where('id', id).then(res => {
+  console.log(`Trying to fetch member with id ` + id + " host name is : " + hostname)
+  await knex('member').select('*').where('id', id).then(res => {
     var member = new Member({}).from(res[0]);
     ctx.state.data = {
       'member': member
@@ -17,11 +20,11 @@ async function create(ctx, next) {
   console.log('trying to create member: ' + JSON.stringify(ctx.request.body));
   var member = ctx.request.body;
   // populate system default values for read only fields
-  member.createdAt = mysql.fn.now();
-  member.updatedAt = mysql.fn.now();
+  member.createdAt = knex.fn.now();
+  member.updatedAt = knex.fn.now();
   member.isSystemAdmin = 'N';
   var memberObj = new Member(member);
-  await mysql('member').insert(memberObj.toInput()).then(res => {
+  await knex('member').insert(memberObj.toInput()).then(res => {
     ctx.state.data = { msg: 'member is ' + JSON.stringify(res) };
   });
 }
@@ -34,12 +37,12 @@ async function update(ctx, next) {
   delete memberPatch.isSystemAdmin;
   delete memberPatch.createdAt;
   // populate system default values for read only fields
-  memberPatch.updatedAt = mysql.fn.now();
+  memberPatch.updatedAt = knex.fn.now();
 
-  await mysql('member').select('*').where('id', id).then(res => {
+  await knex('member').select('*').where('id', id).then(res => {
     var member = new Member({}).from(res[0]);
     member.update(memberPatch);
-    mysql('member').where('id', id).update(member.toInput()).then(res => {
+    knex('member').where('id', id).update(member.toInput()).then(res => {
       ctx.state.data = { msg: 'member is ' + JSON.stringify(res) };
     });
   });
