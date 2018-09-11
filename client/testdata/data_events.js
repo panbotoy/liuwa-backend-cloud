17d8ba732232407f6c043f71723ff43ba80fc62a
Stella Mao <stmeow@users.noreply.github.com>
2018-09-10 22:19:30 -0700

    Styling the home page.

--- client/testdata/data_events.js
+++ client/testdata/data_events.js
@@ -11,11 +11,20 @@ var movies = {
         "images": {
           "small": "http://www.petspet.org/wp-content/uploads/2015/06/Fat-cat-1.png",
         },
-        "title": "hello",
-        "original_title": "hello again",
-        "year": "2012",
+        "title": "9/15 Sushi Buffet",
+        "original_title": "幸福的小周末，跟姐妹们一起来聚会吧！",
       }
-    }    
+    },
+    {
+      "subject": {
+        "id": "2002",
+        "images": {
+          "small": "http://www.petspet.org/wp-content/uploads/2015/06/Fat-cat-1.png",
+        },
+        "title": "9/21 玫瑰园野餐",
+        "original_title": "接叶连枝千万绿，一花两色浅深红。玫瑰园花开正艳，快来一起遛娃吧！",
+      }
+    }   
   ]
 }
 
