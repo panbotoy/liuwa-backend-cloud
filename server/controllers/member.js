7198aea26a04dc1626450280ac60ece4de20e16d
Bo Pan <bopan@bos-macbook-pro.lan>
2018-09-16 19:18:59 -0700

    Abstracted out memberService class for future usage. updated comments for previously created classes

--- server/controllers/member.js
+++ server/controllers/member.js
@@ -1,49 +1,42 @@
-// const { mysql } = require('../qcloud')
 const bodyParser = require('body-parser')
 const Member = require('../models/member.js').Member
-const dbInstance = require('../ds/dbInstance.js').dbInstance.getInstance
-const os = require('os');
+const memberService = require('../services/memberService.js')
 
+/**
+ * Get member data for an Id. The id is in the query params.
+ */
 async function get(ctx, next) {
   var id = parseFloat(ctx.params.id);
-  console.log(`Trying to fetch member with id ` + id + " process is : " + JSON.stringify(process.env))
-  await dbInstance(ctx)('member').select('*').where('id', id).then(res => {
-    var member = new Member({}).from(res[0]);
+  console.log(`Trying to fetch member with id ` + id)
+  await memberService.get(ctx, id).then(member => {
     ctx.state.data = {
       'member': member
     };
   });
 }
 
+/**
+ * Create a member in the system. The memberId will be system auto-generated.
+ */
 async function create(ctx, next) {
   console.log('trying to create member: ' + JSON.stringify(ctx.request.body));
   var member = ctx.request.body;
-  // populate system default values for read only fields
-  member.createdAt = dbInstance(ctx).fn.now();
-  member.updatedAt = dbInstance(ctx).fn.now();
-  member.isSystemAdmin = 'N';
-  var memberObj = new Member(member);
-  await dbInstance(ctx)('member').insert(memberObj.toInput()).then(res => {
+  await memberService.create(ctx, member).then(res => {
     ctx.state.data = { msg: 'member is ' + JSON.stringify(res) };
   });
 }
 
+/**
+ * Update a member given memberId with the patch data provided in the Http request body.
+ */
 async function update(ctx, next) {
   var id = parseFloat(ctx.params.id);
   console.log('trying to update member : ' + id + " with patch: " + JSON.stringify(ctx.request.body));
   var memberPatch = ctx.request.body;
-  // remove system default values
-  delete memberPatch.isSystemAdmin;
-  delete memberPatch.createdAt;
-  // populate system default values for read only fields
-  memberPatch.updatedAt = dbInstance(ctx).fn.now();
-
-  await dbInstance(ctx)('member').select('*').where('id', id).then(res => {
+  
+  await memberService.update(ctx, id, memberPatch).then(res => {
     var member = new Member({}).from(res[0]);
-    member.update(memberPatch);
-    dbInstance(ctx)('member').where('id', id).update(member.toInput()).then(res => {
-      ctx.state.data = { msg: 'member is ' + JSON.stringify(res) };
-    });
+    ctx.state.data = { msg: 'member is ' + JSON.stringify(res) };
   });
 }
 
---
+++ server/services/memberService.js
@@ -0,0 +1,65 @@
+const dbInstance = require('../ds/dbInstance.js').dbInstance.getInstance
+const Member = require('../models/member.js').Member
+
+/**
+ * This class handles business logic + database operations related to {@link Member}
+ * 
+ * It abstracts away the operation with DB from controllers, and allows create a class 
+ * calling multiple services to accomplish more complicated operations.
+ * 
+ */
+class MemberService {
+  constructor() {
+  }
+
+  /**
+   * Get a member given a memberId
+   */
+  async get(ctx, memberId) {
+    var id = parseFloat(ctx.params.id);
+    return await dbInstance(ctx)('member').select('*').where('id', id).then(res => {
+      var member = new Member({}).from(res[0]);
+      return member;
+    });
+  }
+
+  /**
+   * create a member with the given member json rawInput
+   */
+  async create(ctx, member) {
+    // populate system default values for read only fields
+    member.createdAt = dbInstance(ctx).fn.now();
+    member.updatedAt = dbInstance(ctx).fn.now();
+    member.isSystemAdmin = 'N';
+    var memberObj = new Member(member);
+    return await dbInstance(ctx)('member').insert(memberObj.toInput()).then(res => {
+      return res;
+    });
+  }
+
+  /**
+   * update a member for the given Id with the member json input
+   */
+  async update(ctx, id, memberPatch) {
+    var id = parseFloat(ctx.params.id);
+    // remove system default values
+    delete memberPatch.isSystemAdmin;
+    delete memberPatch.createdAt;
+    // populate system default values for read only fields
+    memberPatch.updatedAt = dbInstance(ctx).fn.now();
+    return await dbInstance(ctx)('member').select('*').where('id', id).then(res => {
+      var member = new Member({}).from(res[0]);
+      member.update(memberPatch);
+      return dbInstance(ctx)('member').where('id', id).update(member.toInput()).then(res => {
+        return res;
+      });
+    });
+  }
+}
+
+/**
+ * create a single instance of MemberService
+ */
+const memberService = new MemberService();
+
+module.exports = memberService
